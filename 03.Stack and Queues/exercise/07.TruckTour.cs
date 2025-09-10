ad090da27c9c2a7228c4d17124ee2f2144e1f681
Miroslav Borisov <133015578+MiroslavBr@users.noreply.github.com>
2025-09-10 14:54:15 +0300

    exercises file

---
+++ 03.Stack and Queues/exercise/07.TruckTour.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace _07.TruckTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPetrolPumps = int.Parse(Console.ReadLine());
+
+            Queue<PetrolPump> petrolPumpsRoute = new();
+            for (int i = 0; i < numberOfPetrolPumps; i++)
+            {
+                PetrolPump petrolPump = new(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+                petrolPumpsRoute.Enqueue(petrolPump);
+            }
+
+
+            for (int i = 0; i < numberOfPetrolPumps; i++)
+            {
+                int TruckFuel = 0;
+                bool bestRoute = true;
+                foreach (PetrolPump petrolPump in petrolPumpsRoute)
+                {
+                    TruckFuel += petrolPump.Petrol- petrolPump.DistanceToNextPump;
+                    if (TruckFuel < 0)
+                    {
+                        bestRoute = false;
+                        break;
+                    }
+                }
+
+                if (bestRoute)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+                petrolPumpsRoute.Enqueue(petrolPumpsRoute.Dequeue());
+            }
+        }
+    }
+
+
+    public class PetrolPump
+    {
+        public PetrolPump(int[] info)
+        {
+            int petrol = info[0];
+            int distance = info[1];
+
+            Petrol = petrol;
+            DistanceToNextPump = distance;
+        }
+        public PetrolPump(int petrol, int distance)
+        {
+            Petrol = petrol;
+            DistanceToNextPump = distance;
+        }
+        public int Petrol { get; set; }
+        public int DistanceToNextPump { get; set; }
+    }
+}
+
---
+++ 03.Stack and Queues/exercise/08.BalancedParentheses.cs
@@ -0,0 +1,94 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _08.BalancedParentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            Stack<int> roundBrackets = new();
+            Stack<int> squareBrackets = new();
+            Stack<int> curlyBrackets = new();
+
+            for (int i = 1; i <= input.Length; i++)
+            {
+                switch (input[i - 1])
+                {
+                    case '(':
+                        roundBrackets.Push(i);
+                        break;
+                    case '{':
+                        curlyBrackets.Push(i);
+                        break;
+                    case '[':
+                        squareBrackets.Push(i);
+                        break;
+                    default:
+                        switch (input[i - 1])
+                        {
+                            case ')':
+
+                                if (!AreTheParenthesesBalanced(roundBrackets, i))
+                                {
+                                    return;
+                                }
+
+                                break;
+
+                            case '}':
+                                if (!AreTheParenthesesBalanced(curlyBrackets, i))
+                                {
+                                    return;
+                                }
+
+                                break;
+                            case ']':
+                                if (!AreTheParenthesesBalanced(squareBrackets, i))
+                                {
+                                    return;
+                                }
+
+                                break;
+                        }
+                        break;
+                }
+
+
+            }
+            if (roundBrackets.Count == 0 && 
+                squareBrackets.Count == 0 && 
+                curlyBrackets.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                PrintNo();
+            }
+
+        }
+
+        static bool AreTheParenthesesBalanced(Stack<int> OpenIndexes, int closingIndex)
+        {
+            if ((OpenIndexes.Count > 0) && ((OpenIndexes.Peek() - closingIndex) % 2 != 0))//NotEmpty & Odd
+            {
+                OpenIndexes.Pop();
+                return true;
+            }
+            else
+            {
+                PrintNo();
+                return false;
+            }
+        }
+
+
+        static void PrintNo()
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
