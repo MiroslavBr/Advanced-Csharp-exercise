135603e48bf64647db73d80d721506dd3d47ab76
Miroslav Borisov <133015578+MiroslavBr@users.noreply.github.com>
2025-09-10 10:55:25 +0300

    exercise files

---
+++ 03.Stack and Queues/exercise/01.BasicStackOperations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int pushCount = input[0];
+            int popCount = input[1];
+            int numberX = input[2];
+
+            Stack<int> stackOfNumbers = new(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            for (int i = 0; i < popCount; i++)
+            {
+                stackOfNumbers.Pop();
+            }
+
+            if (stackOfNumbers.Contains(numberX))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                //int[] arr = stackOfNumbers.ToArray().Order().ToArray();
+                //stackOfNumbers = new(arr);
+                //Console.WriteLine(stackOfNumbers.Peek());
+
+                Console.WriteLine(stackOfNumbers.Min());
+            }
+
+
+        }
+    }
+}
---
+++ 03.Stack and Queues/exercise/03.MaximumAndMinimumElement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MaximumAndMinimumElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            Stack<int> stackOfNumbers = new();
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string command = Console.ReadLine();
+                if (command[0] == '1')
+                {
+                    string[] breakDownOfComand = command.Split();
+                    int numberToPush = int.Parse(breakDownOfComand[1]);
+                    stackOfNumbers.Push(numberToPush);
+                }
+                else if (command == "2")
+                {
+                    stackOfNumbers.Pop();
+                }
+                else if (command == "3" && stackOfNumbers.Count != 0)
+                {
+                    Console.WriteLine(stackOfNumbers.Max());
+                }
+                else if (command == "4" && stackOfNumbers.Count != 0)
+                {
+                    Console.WriteLine(stackOfNumbers.Min());
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", stackOfNumbers));
+        }
+    }
+}
