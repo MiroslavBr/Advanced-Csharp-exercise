15a344b23dcea7baff300d7db738415d96d7ed23
Miroslav Borisov <133015578+MiroslavBr@users.noreply.github.com>
2025-09-14 21:41:44 +0300

    exercises files

---
+++ 02.Multidimensional Arrays/lab/06.Jagged-ArrayModification.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.Jagged_ArrayModification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedArray = new int[int.Parse(Console.ReadLine())][];
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                jaggedArray[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+
+            string[] command = Console.ReadLine().Split();
+            while (command[0] != "END")
+            {
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+                int value = int.Parse(command[3]);
+                if (row >= jaggedArray.Length|| row<0)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                else
+                {
+                    if (col >= jaggedArray[row].Length || col<0)
+                    {
+                        Console.WriteLine("Invalid coordinates");
+
+                    }
+                    else
+                    {
+                        if (command[0] == "Add")
+                        {
+                            jaggedArray[row][col] += value;
+
+                        }
+                        else if (command[0] == "Subtract")
+                        {
+                            jaggedArray[row][col] -= value;
+
+                        }
+                    }
+                }
+                    command = Console.ReadLine().Split();
+            }
+
+            foreach (int[] row in jaggedArray)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
---
+++ 02.Multidimensional Arrays/lab/07.PascalTriangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long[][] matrix = new long[int.Parse(Console.ReadLine())][];
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                matrix[row] = new long[row+1];
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (col == 0 || col == matrix[row].Length-1)
+                    {
+                        matrix[row][col] = 1;
+                    }
+                    else
+                    {
+                        matrix[row][col] = matrix[row-1][col - 1] + matrix[row-1][col];
+                    }
+                }
+                //Console.WriteLine(string.Join(" ", matrix[row]));
+            }
+
+            foreach (long[] row in matrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+
+            }
+        }
+    }
+}
