f1730d8ae9a781c14ff2c0b003a883b43b1bb309
Miroslav Borisov <133015578+MiroslavBr@users.noreply.github.com>
2025-09-15 23:10:54 +0300

    exercises files

---
+++ 01.Stack and Queues/exercises/11.KeyRevolver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.KeyRevolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int gubBarrelSize = int.Parse(Console.ReadLine());
+            Stack<int> bullets = new(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Stack<int> locks = new(Console.ReadLine().Split().Select(int.Parse).ToArray().Reverse());
+            int valueOfIntelligence = int.Parse(Console.ReadLine());
+
+
+            int countOfUsedBullets = 0;
+            while (bullets.Count > 0 && locks.Count > 0)
+            {
+
+                int currentBullet = bullets.Peek();
+                int currentLock = locks.Peek();
+                if (currentBullet <= currentLock)
+                {
+                    Console.WriteLine("Bang!");
+                    bullets.Pop(); locks.Pop();
+                    countOfUsedBullets++;
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                    bullets.Pop();
+                    countOfUsedBullets++;
+                }
+                if (countOfUsedBullets % gubBarrelSize == 0&&bullets.Count>0)
+                {
+                    Console.WriteLine("Reloading!");
+                }
+
+
+            }
+
+            if (bullets.Count == 0 && locks.Count > 0)
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+            else
+            {
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${valueOfIntelligence - (bulletPrice * countOfUsedBullets)}");
+            }
+
+        }
+    }
+}
---
+++ 01.Stack and Queues/exercises/12.CupsAndBottles.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _12.CupsAndBottles
+{
+    internal class Program
+    {
+        static int wastedLittersOfWater { get; set; }
+        static void Main(string[] args)
+        {
+            Stack<int> cupsCapacity = new(Console.ReadLine().Split().Select(int.Parse).ToArray().Reverse());
+            Stack<int> waterBottles = new(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            //int wastedLittersOfWater = 0;
+            while (cupsCapacity.Count > 0 && waterBottles.Count > 0)
+            {
+                CupToWatterBottleRatioCheker(cupsCapacity, waterBottles );
+            }
+
+            PrintOutput(cupsCapacity, waterBottles );
+        }
+
+        private static void PrintOutput(Stack<int> cupsCapacity, Stack<int> waterBottles)
+        {
+            if (cupsCapacity.Count > 0)
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cupsCapacity)}");
+            }
+            else
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", waterBottles)}");
+            }
+            Console.WriteLine($"Wasted litters of water: {wastedLittersOfWater}");
+        }
+
+        static void CupToWatterBottleRatioCheker(Stack<int> cupsCapacity, Stack<int> waterBottles)
+        {
+            int currentCupCapacity = cupsCapacity.Peek();
+            int currentWaterBotlle = waterBottles.Peek();
+
+            if (currentCupCapacity >= currentWaterBotlle)
+            {
+                currentCupCapacity = PoringIntoBiggerCup(cupsCapacity, waterBottles, currentCupCapacity, currentWaterBotlle);
+            }
+            else
+            {
+                wastedLittersOfWater = PoringIntoSmallerCup(cupsCapacity, waterBottles, wastedLittersOfWater, currentCupCapacity, currentWaterBotlle);
+            }
+        }
+
+        private static int PoringIntoSmallerCup(Stack<int> cupsCapacity, Stack<int> waterBottles, int wastedLittersOfWater, int currentCupCapacity, int currentWaterBotlle)
+        {
+            waterBottles.Pop();
+            cupsCapacity.Pop();
+            wastedLittersOfWater += currentWaterBotlle - currentCupCapacity;
+            return wastedLittersOfWater;
+        }
+
+        private static int PoringIntoBiggerCup(Stack<int> cupsCapacity, Stack<int> waterBottles, int currentCupCapacity, int currentWaterBotlle)
+        {
+            waterBottles.Pop();
+            currentCupCapacity -= currentWaterBotlle;
+            if (currentCupCapacity == 0)
+            {
+                cupsCapacity.Pop();
+            }
+            else
+            {
+                cupsCapacity.Pop();
+                cupsCapacity.Push(currentCupCapacity);
+            }
+
+            return currentCupCapacity;
+        }
+    }
+}
