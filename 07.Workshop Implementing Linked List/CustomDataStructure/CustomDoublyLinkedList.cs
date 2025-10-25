013a2ace46ebb366f258415fdcf38d6ee74a3f95
Miroslav Borisov <133015578+MiroslavBr@users.noreply.github.com>
2025-10-25 21:14:11 +0300

    changes

---
+++ 07.Workshop Implementing Linked List/CustomDataStructure/CustomDoublyLinkedList.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CustomDataStructure
+{
+    public class DoublyLinkedList
+    {
+        public class ListNode
+        {
+            public int Value { get; set; }
+            public ListNode Next { get; set; }
+            public ListNode Previous { get; set; }
+            public ListNode(int value)
+            {
+                this.Value = value;
+            }
+        }
+
+        public ListNode Head { get; set; }
+        public ListNode Tail { get; set; }
+
+        public int Count { get; private set; }
+
+
+        public void AddFirst(int value)
+        {
+            ListNode newHead = new(value);
+            if (this.Head == null)
+            {
+                this.Head = newHead;
+                this.Tail = newHead;
+                Count++;
+                return;
+
+            }
+
+            newHead.Next = this.Head;
+            this.Head.Previous = newHead;
+            this.Head = newHead;
+            Count++;
+        }
+        public void AddLast(int value)
+        {
+            ListNode newTail = new(value);
+
+            if (this.Tail == null)
+            {
+                this.Head = newTail;
+                this.Tail = newTail;
+                Count++;
+
+                return;
+            }
+
+            this.Tail.Next = newTail;
+            newTail.Previous = this.Tail;
+            this.Tail = newTail;
+            Count++;
+        }
+        public int RemoveFirst()
+        {
+            if (this.Count==0)
+            {
+                throw new InvalidOperationException("This list is empty");
+            }
+
+
+            int valueOfRemovedElement = this.Head.Value;
+            this.Head = this.Head.Next;
+
+            if (this.Head == null)
+            {
+                this.Tail = null;
+            }
+            else
+            {
+                this.Head.Previous = null;
+            }
+            Count--;
+            return valueOfRemovedElement;
+        }
+        public int RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("This list is empty");
+            }
+
+            int valueOfRemovedElement = this.Tail.Value;
+            this.Tail = this.Tail.Previous;
+
+            if(this.Tail == null)
+            {
+                this.Head = null;
+            } else
+            {
+                this.Tail.Next = null;
+            }
+
+            Count--;
+            return valueOfRemovedElement;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            ListNode currentNode = this.Head;
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.Next;
+            }
+        }
+        public int[] ToArray()
+        {
+            int[] array = new int[this.Count];
+            ListNode currentNode = this.Head;
+            for (int i = 0; i < this.Count; i++)
+            {
+                array[i] = currentNode.Value;
+                currentNode = currentNode.Next;
+            }
+            return array;
+        }
+    }
+}
---
+++ 07.Workshop Implementing Linked List/CustomDataStructure/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CustomDataStructure;
+using static CustomDataStructure.DoublyLinkedList;
+
+namespace CustomDoublyLinkedList
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList list = new();
+
+            list.AddFirst(3);
+            list.AddFirst(2);
+            list.AddFirst(1);
+            list.AddLast(4);
+            list.AddLast(5);
+
+            list.RemoveFirst();
+            list.RemoveLast();
+           
+            list.ForEach(x => Console.WriteLine(x));
+
+            int[] nums = list.ToArray();
+            nums =nums.Select(x => x += 1).ToArray();
+            Console.WriteLine(string.Join(", ", nums)); 
+
+        } 
+
+        
+    }
+}
